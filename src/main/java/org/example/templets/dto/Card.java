319eb1c688b6275149e0ba3982deb6d603bc0f1e
이종환 (인턴) <ghksqkdwod0@innogrid.com>
2025-12-22 12:04:36 +0900

    인디언 포커 완성

---
+++ src/main/java/org/example/templets/dto/Card.java
@@ -0,0 +1,16 @@
+package org.example.templets.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class Card {
+    private String suit; // S, D, H, C
+    private int rank;    // 1(A) ~ 13(K)
+
+    @Override
+    public String toString() {
+        return suit + rank;
+    }
+}
